20798b74a899a8ef43e505961080d5bbe82fa0b0
kevinvanherf <kevin.vanherf@gmail.com>
2022-10-25 21:28:25 +0200

    en stuent en gebruiker toegevoegt in de dat aseed voor ind e dat base

--- HogeschoolPXL/HogeschoolPXL/Data/DefaultData/SeeData.cs
+++ HogeschoolPXL/HogeschoolPXL/Data/DefaultData/SeeData.cs
@@ -1,4 +1,6 @@
-﻿namespace HogeschoolPXL.Data.DefaultData
+﻿using HogeschoolPXL.Models;
+
+namespace HogeschoolPXL.Data.DefaultData
 {
     public class SeeData
     {
@@ -6,10 +8,15 @@
         {
             using (var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<HogeschoolPXLDbContext>())
             {
+                if (!context.Gebruiker.Any())
+                {
+                    new Gebruiker { GebruikerId = 1, Naam = "Vanherf", VoorNaam="Kevin", Email="Kevin.vanherf@icloud.com" };
+                }
+                
                 //context.database.ens
                 if (!context.Student.Any())
                 {
-
+                    new Student { GebruikerId = 1 , StudentId= 12100954};
                 }
             }
         }
