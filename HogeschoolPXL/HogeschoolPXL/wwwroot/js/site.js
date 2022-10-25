5c8cba3a58c3159cf3e11a9bc572024d2d145f3d
kevinvanherf <kevin.vanherf@gmail.com>
2022-10-25 20:47:29 +0200

    test fetch

--- HogeschoolPXL/HogeschoolPXL/wwwroot/js/site.js
+++ HogeschoolPXL/HogeschoolPXL/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//test voor api te gebruiken wanner het moet 
+//const apiUrl = 'https://localhost:44315/api/'; //VOOR WINDOWS
+
+//fetch(apiUrl + 'auto', {
+//    method: 'GET', // or 'PUT'
+//    headers: {
+//        'Content-Type': 'application/json',
+//    },
+//})
+//    .then(response => response.json())
+//    .then(data => {
+//        const empty = {};
+//        if (data == 0) {
+//            FunctieFoutMelding("Probeer Opnieuw");
+//        } else {
+//            myArray = data;
+            
+
+//        }
+//    })
+//    .catch((error) => {
+//        FunctieFoutMelding("Probeer Opnieuw");
+//    });
