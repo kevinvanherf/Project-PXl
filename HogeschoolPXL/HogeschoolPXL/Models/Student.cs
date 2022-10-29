49cdba447082c6d7221528dff908622a24062af7
kevinvanherf <kevin.vanherf@gmail.com>
2022-10-29 18:23:23 +0200

    aan passigne poject

--- HogeschoolPXL/HogeschoolPXL/Models/Student.cs
+++ HogeschoolPXL/HogeschoolPXL/Models/Student.cs
@@ -1,7 +1,10 @@
-﻿namespace HogeschoolPXL.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HogeschoolPXL.Models
 {
     public class Student
     {
+        [Required]
         public int StudentId { get; set; }
         public int GebruikerId { get; set; }
     }
