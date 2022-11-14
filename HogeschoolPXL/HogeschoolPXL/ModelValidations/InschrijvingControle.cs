a75d8ed43e9ce488722e6fd9a75032a06cb4199f
kevinvanherf <kevin.vanherf@gmail.com>
2022-11-14 22:24:40 +0100

    aanpassing inschrijving modellelati wegens niet werkt

--- HogeschoolPXL/HogeschoolPXL/ModelValidations/InschrijvingControle.cs
+++ HogeschoolPXL/HogeschoolPXL/ModelValidations/InschrijvingControle.cs
@@ -5,24 +5,15 @@ namespace HogeschoolPXL.ModelValidations
 {
     public class InschrijvingControle : Attribute, IModelValidator
     {
-        public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context , HogeschoolPXLDbContext contextdb)
+        public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context )
         {
             
             var lst = new List<ModelValidationResult>();
-            if (contextdb.Student == null && contextdb.VakLector == null)
+            if (context.Model == null )
             {
-                lst.Add(new ModelValidationResult("", "er bestaat geen leerlingen en geen curssen dus kun je geen inschrijving  maken , er moeten eerst leerlingen komen en cursussen worden aangemaakt."));
+                lst.Add(new ModelValidationResult("", "er bestaat geen leerlingen of geen curssen dus kun je geen inschrijving  maken , er moeten eerst leerlingen komen of cursussen worden aangemaakt."));
             }
-            else if (contextdb.Student == null)
-            {
-                lst.Add(new ModelValidationResult("", "er bestaat geen leerlingen dus kun je geen inschrijving  maken , er moeten eerst leerlingen komen."));
-
-            }
-            else if (contextdb.VakLector == null)
-            {
-                lst.Add(new ModelValidationResult("", "er bestaat geen cursussen dus kun je geen inschrijving  maken ,maak eerst een cursus "));
-
-            }
+          
             return lst;
 
         }
