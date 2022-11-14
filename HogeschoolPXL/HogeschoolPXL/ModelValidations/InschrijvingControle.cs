20b5979e53353136f25255556a930c7d6c0a10eb
kevinvanherf <kevin.vanherf@gmail.com>
2022-11-14 20:52:03 +0100

    isnchrijving model validation aangepast dat het te goei werk hoe gevraagt word

--- HogeschoolPXL/HogeschoolPXL/ModelValidations/InschrijvingControle.cs
+++ HogeschoolPXL/HogeschoolPXL/ModelValidations/InschrijvingControle.cs
@@ -1,18 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+﻿using HogeschoolPXL.Data;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace HogeschoolPXL.ModelValidations
 {
     public class InschrijvingControle : Attribute, IModelValidator
     {
-        public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context)
+        public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context , HogeschoolPXLDbContext contextdb)
         {
-            
             var lst = new List<ModelValidationResult>();
-            if (context.Model == null)
+            if (contextdb.Student == null && contextdb.VakLector == null)
             {
-                lst.Add(new ModelValidationResult("", "er bestaat geen vakken en of geen leerlingen  dus kun je geen vak maken "));
+                lst.Add(new ModelValidationResult("", "er bestaat geen leerlingen en geen curssen dus kun je geen inschrijving  maken , er moeten eerst leerlingen komen en cursussen worden aangemaakt."));
+            }
+            else if (contextdb.Student == null)
+            {
+                lst.Add(new ModelValidationResult("", "er bestaat geen leerlingen dus kun je geen inschrijving  maken , er moeten eerst leerlingen komen."));
 
             }
+            else if (contextdb.VakLector == null)
+            {
+                lst.Add(new ModelValidationResult("", "er bestaat geen cursussen dus kun je geen inschrijving  maken ,maak eerst een cursus "));
+
+            }
+
+
 
             return lst;
 
