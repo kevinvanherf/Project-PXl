19c414473a86fbb1332bcc4717e7f0e04aea2020
kevinvanherf <kevin.vanherf@gmail.com>
2022-11-21 20:56:47 +0100

    identiy user aan gepast nog niet gemplementeert

---
+++ HogeschoolPXL/HogeschoolPXL/Models/ViewModels/Identity/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace HogeschoolPXL.Models.ViewModels.Identity
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
